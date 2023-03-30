9865740ad89a79f0aa9a8f31bb39fa87a90a70a7
Ataliya Aarfeen <89702899+Ataliya-01@users.noreply.github.com>
2023-03-30 17:49:01 +0530

    Sorting1

---
+++ ImplementingSelectionSort.java
@@ -0,0 +1,72 @@
+/* Implementing Selection Sort
+
+Given an array arr of size n, containing positive integers. You need to sort the elements of array using the Selection Sort algorithm.
+
+Input Format
+First line contains an integer n which is the size of array arr
+
+Second line contains n space separated integers of arr
+
+Output Format
+Return the sorted array
+
+Example 1
+Input
+
+5
+4 1 3 9 7
+Output
+
+1 3 4 7 9
+Explanation
+
+The array after performing Selection sort: 1 3 4 7 9.
+
+Example 2
+Input
+
+10
+10 9 8 7 6 5 4 3 2 1
+Output
+
+1 2 3 4 5 6 7 8 9 10
+Constraints
+1 <= n <= 104
+
+-106 <= A[i] <= 106
+
+ */
+
+
+import java.util.*;
+
+public class ImplementingSelectionSort {
+    public static int[] SelectionSort(int[] arr) {
+        // Write your code here
+		for(int i = 0; i<arr.length-1; i++){
+			int smallest = i;
+			for(int j = i+1; j<arr.length; j++){
+				if(arr[smallest]>arr[j]){
+				smallest = j;
+			}
+		}
+		int temp = arr[smallest];
+		arr[smallest] = arr[i];
+		arr[i] = temp;
+    }
+		return arr;
+	}
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int[] ans = SelectionSort(arr);
+
+        for(int a : ans)
+            System.out.print(a + " ");
+    }
+}
---
+++ MaximumOccurrence.java
@@ -0,0 +1,85 @@
+/* Maximum occurrence
+You are given an array arr of length n. Your task is to print the element which has maximum occurrences in the array. If two elements have the same occurrence then print the smaller element
+
+Input Format
+The first line of input contains an integer n, size of the array.
+
+The next line contains n space seperated integers denoting the elements of the array.
+
+Output Format
+Print single integer denoting the maximum occurring element in the array.
+
+Example 1
+Input
+
+8
+7 7 6 4 8 7 3 1
+Output
+
+7
+Explanation
+
+We have arr = [7 7 6 4 8 7 3 1]
+
+As 7 is occurring 3
+
+Example 2
+Input
+
+7
+9 8 1 1 2 2 3
+Output
+
+1
+Explanation
+
+1 and 2 have smae occurrence that is 2 so print smaller element i.e. 1.
+
+Constraints
+1 <= n <= 10^6
+
+1 <= arr[i] <= 10^9
+ */
+
+
+
+import java.util.*;
+
+public class MaximumOccurrence {
+    static int maximum_occurrence(int arr[], int n) {
+        //Write code here
+		// if(arr.length<1){
+		// 	retrun -1;
+		// }
+		
+		Arrays.sort(arr);
+		int maxcount = 0;
+	    int element_having_max_freq = 0;
+	    for (int i = 0; i < n; i++) {
+	      int count = 0;
+	      for (int j = 0; j < n; j++) {
+	        if (arr[i] == arr[j]) {
+	          count++;
+        }
+      }
+ 
+	      if (count > maxcount) {
+	        maxcount = count;
+	        element_having_max_freq = arr[i];
+      }
+    }
+ 
+    return element_having_max_freq;
+  }
+    public static void main(String args[]) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(maximum_occurrence(arr, n));
+
+    }
+}
